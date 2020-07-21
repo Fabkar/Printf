9aea132d5bd06b7f4cb41a47163af11245e05b96
Daniel <2057@holbertonschool.com>
2020-07-21 17:44:02 -0500

    p_int prints integer to standard output

---
+++ function_int.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+* p_int - print variable type integer.
+* @arguments: arguments
+* Return: Integer.
+*/
+int p_int(va_list arguments)
+{
+	int lenght, n, cont = 1;
+​
+	n = va_arg(arguments, int);
+	lenght = p_aux_int(n, cont);
+	return (lenght);
+}
+/**
+* p_aux_int - Recursion function aux and print int.
+* @n: Number to print (lenght).
+* @cont: Counter to string.
+* Return: lenght of the "int".
+*/
+int p_aux_int(int n, int cont)
+{
+	++cont;
+	if (n < 0)
+	{
+		n = -n, _putchar('-'), ++cont;
+	}
+	if (n / 10)
+	{
+		p_aux_int(n / 10, cont);
+	}
+	_putchar(n % 10 + '0');
+	return (cont);
+}
