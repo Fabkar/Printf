2561fc54d0ecac2b5593e380b31d4cfd733b4c45
Fabkar <2003@holbertonschool.com>
2020-07-21 21:23:52 -0500

    update conditional when pointer is NULL

--- function_str.c
+++ function_str.c
@@ -10,6 +10,11 @@ int p_str(va_list arguments)
 	int a;
 
 	p = va_arg(arguments, char *);
+	if (p == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
 	for (a = 0 ; p[a] != '\0' ; a++)
 	{
 	_putchar(p[a]);
