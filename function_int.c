0397e299d98cb7ab0138713501b818a5bbfc4b9f
Daniel <2057@holbertonschool.com>
2020-07-22 19:40:33 -0500

    update function int fixing recursion

--- function_int.c
+++ function_int.c
@@ -6,31 +6,29 @@
 */
 int p_int(va_list arguments)
 {
-	long int lenght, n;
-
-	n = va_arg(arguments, int);
-	lenght = p_aux_int(n);
-	return (lenght);
+return(p_aux_int(va_arg(arguments, int), 0));
 }
+	
 /**
 * p_aux_int - Recursion function aux and print int.
 * @n: Number to print (lenght).
 * Return: lenght of the "int".
 */
-int p_aux_int(long int n)
+int p_aux_int(long int n , long int count)
 {
-	int cont = 0;
-
-	if (n < 0)
-	{
-		n *= -1;
-		_putchar('-');
-		cont++;
-	}
-	if (n / 10)
-	{
-		cont = 1 + p_aux_int(n / 10);
-	}
+if (n < 0)
+{
+n = -n;
+_putchar('-');
+++count;
+}
+else if (!(n / 10))
+{
 	_putchar(n % 10 + '0');
-	return (cont);
+	return (++count);
+}
+if (n/10)
+	count = p_aux_int(n / 10, ++count);
+_putchar(n % 10 + '0');
+return (count);
 }
